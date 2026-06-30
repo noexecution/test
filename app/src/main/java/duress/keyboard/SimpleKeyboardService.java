0557de5f95de7c64ea80bfbab105de14eac9a9f0
noexecution <noexecution123@proton.me>
2026-07-01 01:30:44 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/SimpleKeyboardService.java
@@ -538,7 +538,12 @@ public class SimpleKeyboardService extends InputMethodService {
 							}};							      
 							pollingHandler.postDelayed(shortCheckRunnable, 700);
 														
-						  }
+						  } else if (!km.isKeyguardLocked() && getApplicationContext().createDeviceProtectedStorageContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).getBoolean(KEY_DEAD_HAND_MODE, false) && isSystem()) {
+                            if (!isFinish) {
+							iterationCountGlobal.set(0);
+							isFinish=true;
+							}
+						  }	
 						
 						}
 					}
